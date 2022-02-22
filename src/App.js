7b140a5a3981ccc32b2d7b061a7bfee6d5c06e05
Arya Iranpour <aryairanpour@Aryas-MacBook-Pro-2.local>
2022-02-22 11:53:59 -0500

    minor cleanup

--- src/App.js
+++ src/App.js
@@ -66,24 +66,74 @@ const App = () => {
         .sort(() => 0.5 - Math.random())
     );
   };
-  const checkWord = () => {
-    if ([...word].filter((w) => letters.indexOf(w) === -1).length > 0) {
-      setTimeLeft((timeLeft) => timeLeft - 1);
-      setIncorrect('Wrong letter used');
-    } else {
-      fetch(`//api.dictionaryapi.dev/api/v2/entries/en/${word}`).then((res) => {
-        if (!res.ok) {
-          setIncorrect('This is not a word');
-          setTimeLeft((timeLeft) => timeLeft - 1);
-        } else {
-          setIncorrect('');
-          setGuesses((prev) => [...prev, word]);
-          setScore(score + word.length);
-          setTimeLeft((timeLeft) => timeLeft + word.length);
-          setWord('');
-          randomizeLetters();
+
+  const thankYouNext = () => {
+    setIncorrect('');
+    randomizeLetters();
+    setWord('');
+  };
+
+  const enterPressed = async () => {
+    if (word.length === 0) {
+      thankYouNext();
+    } else if (word.indexOf(',') > -1) {
+      let stillCorrect = true;
+      const multiWordArray = [
+        ...new Set(word.split(',').map((item) => item.trim())),
+      ];
+      for (const wordToTry of multiWordArray) {
+        if (stillCorrect && wordToTry.length > 0) {
+          if (
+            [...wordToTry].filter((w) => letters.indexOf(w) === -1).length > 0
+          ) {
+            setTimeLeft((timeLeft) => timeLeft - 1);
+            setIncorrect('Wrong letter used');
+            stillCorrect = false;
+          } else {
+            await fetch(
+              `//api.dictionaryapi.dev/api/v2/entries/en/${wordToTry}`
+            ).then((res) => {
+              if (!res.ok) {
+                setIncorrect('One of these is not a word');
+                setTimeLeft((timeLeft) => timeLeft - 1);
+                stillCorrect = false;
+              }
+            });
+          }
         }
-      });
+      }
+      if (stillCorrect) {
+        let scoreKeeping = 0;
+
+        multiWordArray.forEach((wordToTry) => {
+          setGuesses((prev) => [...prev, wordToTry]);
+          scoreKeeping += wordToTry.length;
+        });
+        setScore(score + scoreKeeping * multiWordArray.length);
+        setTimeLeft((timeLeft) => timeLeft + scoreKeeping);
+
+        thankYouNext();
+      }
+    } else {
+      if ([...word].filter((w) => letters.indexOf(w) === -1).length > 0) {
+        setTimeLeft((timeLeft) => timeLeft - 1);
+        setIncorrect('Wrong letter used');
+      } else {
+        await fetch(`//api.dictionaryapi.dev/api/v2/entries/en/${word}`).then(
+          (res) => {
+            if (!res.ok) {
+              setIncorrect('This is not a word');
+              setTimeLeft((timeLeft) => timeLeft - 1);
+            } else {
+              setGuesses((prev) => [...prev, word]);
+              setScore(score + word.length);
+              setTimeLeft((timeLeft) => timeLeft + word.length);
+
+              thankYouNext();
+            }
+          }
+        );
+      }
     }
   };
 
@@ -95,9 +145,9 @@ const App = () => {
       <div className="container text-center my-5 game">
         <div className="row">
           <div className="col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
-            <div className="card shadow-lg">
+            <div className="game__card card shadow-lg">
               <div className="card-body">
-                <h1>Unscramb.ly</h1>
+                <h1 className="display-6 pt-4 pb-3">Unscramb.ly</h1>
                 {gameStatus === gameStatuses.New ? (
                   <>
                     <label htmlFor="leadTime" className="form-label">
@@ -111,6 +161,7 @@ const App = () => {
                       value={leadTime}
                       onChange={(e) => setLeadtime(e.target.value)}
                       id="leadTime"
+                      autoFocus={true}
                     />
                     <button
                       className="btn btn-primary btn-block btn-lg"
@@ -121,29 +172,44 @@ const App = () => {
                   </>
                 ) : gameStatus === gameStatuses.Ongoing ? (
                   <>
-                    <p className="text-bold">
-                      {timeLeft} second{timeLeft !== 1 && 's'}
-                    </p>
-                    <p>Score: {score}</p>
-                    <div className="row mx-0 mb-2">
+                    <div className="row mb-3 py-2 border-top border-bottom">
+                      <div className="col-6 text-center">
+                        <small>
+                          <strong>Time</strong>
+                        </small>
+                        <div>
+                          <small>
+                            {timeLeft} second{timeLeft !== 1 && 's'}
+                          </small>
+                        </div>
+                      </div>
+                      <div className="col-6 text-center">
+                        <small>
+                          <strong>Score</strong>
+                        </small>
+                        <div>
+                          <small>{score}</small>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row mx-0 mb-1">
                       {letters.map((l, i) => (
                         <div
                           key={`letter-${i}`}
-                          className={`col text-center border py-2 ${
+                          className={`col text-center border py-2 h3 ${
                             i % 2 !== 0 && 'bg-light'
                           }`}
                         >
-                          {l.toLowerCase()}
+                          <strong>{l.toUpperCase()}</strong>
                         </div>
                       ))}
                     </div>
-
                     <input
                       type="text"
                       className="form-control border border-primary"
                       value={word}
                       onChange={(e) => setWord(e.target.value.toLowerCase())}
-                      onKeyUp={(e) => e.key === 'Enter' && checkWord()}
+                      onKeyUp={(e) => e.key === 'Enter' && enterPressed()}
                     />
                     {incorrect.length > 0 && (
                       <p className="alert alert-warning" role="alert">
